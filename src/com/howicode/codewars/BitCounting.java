8a81b118f9605d660bd5b9387c320c6fcf7fbab0
Jerome Savellano <jerome.savellano@qbryx.com>
2018-10-29 18:25:55 +0800

    Bit Counting

---
+++ src/com/howicode/codewars/BitCounting.java
@@ -0,0 +1,21 @@
+package com.howicode.codewars;
+
+/*
+ * Write a function that takes an integer as input, and returns the number of bits that are equal
+ * to one in the binary representation of the number. You can guarantee the input is non-negative.
+ * 
+ * Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case.
+ * */
+public class BitCounting {
+	
+	public static int countBits(int n) {
+		int bitCount = 0;
+		while(n != 0) {
+			if((n&1) == 1) {
+				bitCount++;
+			}
+			n >>>= 1;
+		}
+		return bitCount;
+	}
+}
